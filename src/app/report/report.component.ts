fee43a9e040f18a29b2ab82f166f493eb3ba0293
unknown <er.nitindinkar@gmail.com>
2023-11-01 14:02:17 +0530

    attendence report

--- src/app/report/report.component.ts
+++ src/app/report/report.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-report',
   templateUrl: './report.component.html',
   styleUrls: ['./report.component.scss']
 })
-export class ReportComponent {
-
+export class ReportComponent implements OnInit{
+ngOnInit(): void {
+  $.getScript('../../assets/js/app.js');
+    $.getScript('../../assets/js/select2.min.js');
+    $.getScript('../../assets/js/jquery-ui.min.js');
+}
 }
