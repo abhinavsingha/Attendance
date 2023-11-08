cec7bc33e85789f6923f5d81c1a7f1cd3db022d3
unknown <er.nitindinkar@gmail.com>
2023-11-08 12:52:07 +0530

    slide bar

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -3,6 +3,30 @@
 $(document).ready(function() {
 
 	
+
+	$(".hamburger-menu").on("click", function(e) {
+        e.preventDefault();
+        $(".slide-bar").toggleClass("show");
+        $("body").addClass("on-side");
+        $('.body-overlay').addClass('active');
+        $(this).addClass('active');
+    });
+
+    $(".close-mobile-menu > a").on("click", function(e) {
+        e.preventDefault();
+        $(".slide-bar").removeClass("show");
+        $("body").removeClass("on-side");
+        $('.body-overlay').removeClass('active');
+        $('.hamburger-menu > a').removeClass('active');
+    });
+
+    $('.body-overlay').on('click', function() {
+        $(this).removeClass('active');
+        $(".slide-bar").removeClass("show");
+        $("body").removeClass("on-side");
+        $('.hamburger-menu > a').removeClass('active');
+    });
+
 	// Variables declarations
 
 	var $wrapper = $('.main-wrapper');
