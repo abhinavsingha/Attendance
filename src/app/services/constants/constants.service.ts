8739ee410cb0c499d76b92a75f8ad8b788c3bf9c
abhinav singha <abhinavsingha@outlook.com>
2023-11-21 11:21:11 +0530

    icard

--- src/app/services/constants/constants.service.ts
+++ src/app/services/constants/constants.service.ts
@@ -7,8 +7,8 @@ export class ConstantsService {
   constructor() {}
 
 //  serviceUrl = 'http://192.168.2.2:8080/'; //UAT server
-  // serviceUrl = 'http://192.168.137.126:8080/';
-  serviceUrl = 'http://petregistration.mynoida.co.in/NaprWeb/'; //UAT server
+  serviceUrl = 'http://172.20.10.4:8081/';
+  // serviceUrl = 'http://petregistration.mynoida.co.in/NaprWeb/'; //UAT server
   api = {
     login:
       this.serviceUrl + 'api/adminLogin',
